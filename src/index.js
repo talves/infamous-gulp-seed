c38ebe52713a60cfd273893c3f68d81e6bd3f12f
Tony Alves <tony@3alves.com>
2015-09-04 14:06:03 -0700

    [initial commit]
    Build the seed!

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Famous dependencies
+import { DOMElement } from 'infamous-engine/src/dom-renderables/DOMElement';
+import { FamousEngine } from 'infamous-engine/src/core/FamousEngine';
+import { SpinnerAnimation as Spinner } from './js/animation/SpinnerAnimation';
+import { LogoNode as Logo } from './js/LogoNode';
+
+// Boilerplate code to make your life easier
+FamousEngine.init();
+
+// Create a scene for the FamousEngine to render
+var scene = FamousEngine.createScene();
+var nextNode = scene.addChild();
+
+// Get a node of the Famous Logo
+var logo = new Logo(nextNode);
+console.log('logonode', logo);
+
+// Setup a custom component for animation
+var animation = new Spinner(logo);
+animation.start();
